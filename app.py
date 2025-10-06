d3bb2719932ddfdb478aaab24e3d29f2d4ab108b
Marie Hallan <marie.hallan@gmail.com>
2025-10-06 22:49:10 +0200

    Code cleaning + simplify code

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent))
 
-from extract.ssb_pxweb import fetch_from_ssb
+from extract.ssb_pxweb import fetch_sickdata_from_ssb
 
 if __name__ == "__main__":
     query = {} # TODO
 
-    ssb_extract = fetch_from_ssb(ssb_table_id, query)
+    ssb_extract = fetch_sickdata_from_ssb(query)
     print(ssb_extract)
     
